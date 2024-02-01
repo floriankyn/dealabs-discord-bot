fdccf976bc5c42455cdef5db25235054f6fa97e5
Florian Lepage <florian.kyn@gmail.com>
2024-02-01 20:49:00 +0100

    Add get deal and remove deal
    Add getdeal query
    add remove deal query
    Fix slug

--- src/config/database.ts
+++ src/config/database.ts
@@ -38,14 +38,50 @@ export const checkIfChannelListening = async (channelId: string): Promise<boolea
 export const saveDealChannel = async (
   channelId: string,
   guildId: string, 
-  authorId: string
+  authorId: string,
+  slug: string,
 ): Promise<void> => {
   try {
     await prisma.deals.create({
       data: {
+        slug: slug,
         channel_id: channelId,
         guild_id: guildId,
         author_id: authorId
+      }
+    });
+  } catch (error) {
+    const errorMessage = error as Error;
+    logError(errorMessage.message);
+  }
+}
+
+export const getDealChannel = async (
+  channelId: string,
+): Promise<Deals | null> => {
+  try {
+    const deal: Deals | null = await prisma.deals.findFirst({
+      where: {
+        channel_id: channelId
+      }
+    });
+
+    return deal;
+  } catch (error) {
+    const errorMessage = error as Error;
+    logError(errorMessage.message);
+  }
+
+  return null;
+}
+
+export const removeDealChannel = async (
+  channelId: string,
+): Promise<void> => {
+  try {
+    await prisma.deals.deleteMany({
+      where: {
+        channel_id: channelId,
       }
     });
   } catch (error) {
