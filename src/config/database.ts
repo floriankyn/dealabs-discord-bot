c64abf8fcda738e07105a9027ea3495a562198e4
Florian Lepage <florian.kyn@gmail.com>
2024-02-01 17:49:21 +0100

    Update database.ts
    Adding listen cmd related queries

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { Deals, PrismaClient } from '@prisma/client';
 
 import { logError, logMessage } from '../lib/logger';
 
@@ -17,3 +17,39 @@ export const connectPrisma = async (): Promise<void> => {
     logError(errorMessage.message);
   }
 };
+
+
+export const checkIfChannelListening = async (channelId: string): Promise<boolean> => {
+  const deal: Deals | null = await prisma.deals.findFirst({
+    where: {
+      channel_id: channelId
+    }
+  });
+
+  if (deal === null) {
+    return true;
+  } else if (deal.channel_id === channelId) {
+    return false;
+  }
+
+  return false;
+}
+
+export const saveDealChannel = async (
+  channelId: string,
+  guildId: string, 
+  authorId: string
+): Promise<void> => {
+  try {
+    await prisma.deals.create({
+      data: {
+        channel_id: channelId,
+        guild_id: guildId,
+        author_id: authorId
+      }
+    });
+  } catch (error) {
+    const errorMessage = error as Error;
+    logError(errorMessage.message);
+  }
+}
