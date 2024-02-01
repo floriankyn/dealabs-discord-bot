942997e5f49d0ef6d77860a8825e05ffe32a5b0a
Florian Lepage <florian.kyn@gmail.com>
2024-02-01 17:10:32 +0100

    Add slug func

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -41,3 +41,19 @@ export const handleError = async (
   }
 };
 
+export const isSlug = (str: string): boolean => {
+  const slugRegex = /^[a-z0-9]+(-[a-z0-9]+)*$/;
+  return slugRegex.test(str);
+}
+
+export const serializeToSlug = (str: string): string => {
+  if (isSlug(str)) {
+    return str;
+  }
+
+  return str
+    .toLowerCase()
+    .trim()
+    .replace(/[\s\W-]+/g, '-')
+    .replace(/^-+|-+$/g, '');
+}
